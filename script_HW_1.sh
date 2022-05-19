83284de2a490382ae2f8226ee9fd39f37f3fa88a
Andrei Bragin <100410326+AndreiBra@users.noreply.github.com>
2022-05-19 13:26:56 +0300

    Update script_HW_1.sh

--- script_HW_1.sh
+++ script_HW_1.sh
@@ -2,22 +2,22 @@
 #2) Написать скрипт который выполнит 
 #автоматически пункты 3, 4, 5, 6, 7, 8, 13
 
-mkdir folder_1
+mkdir foldername_1
 
 #3) Зайти в папку
 cd folder_1
 
 #4) Создать 3 папки
-mkdir folder_1 folder_2 folder_3
+mkdir foldername_1 foldername_2 foldername_3
 
 #5) Зайти в любоую папку
-cd folder_1
+cd foldername_1
 
 #6) Создать 5 файлов (3 txt, 2 json)
 touch file_1.txt file_2.txt file_3.txt file_1.json file_2.json
 
 #7) Создать 3 папки
-mkdir folder_1 folder_2 folder_3
+mkdir foldername_1 foldername_2 foldername_3
 
 #8). Вывести список содержимого папки
 ls
